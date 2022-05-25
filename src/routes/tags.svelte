641648e1a4e8b9dfe942eb5a30cf316be58e7f3e
HerrCraziDev <herrcrazi@gmail.com>
2022-05-25 14:52:35 +0200

    tag placeholder

---
+++ src/routes/tags.svelte
@@ -0,0 +1,10 @@
+
+<script>
+    import { config } from '$lib/config/settings.json'
+</script>
+
+<svelte:head>
+    <title>{config.sitename}</title>
+</svelte:head>
+
+<pre>501 Not Implemented</pre>
